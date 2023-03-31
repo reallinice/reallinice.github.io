8b5413567bd2d427811b70df15d7abfbb9ee6a81
reallinice <reallinice@users.noreply.github.com>
2023-03-31 15:09:56 +0000

    change bb.js url 5d9eafab5dd0fe51bab638744e0a3bac07188568

--- js/bber.js
+++ js/bber.js
@@ -102,7 +102,8 @@ if (nextLength < limit) { document.querySelector("button.button-load").remove() 
             bbMemo.creatorId fetch(bbUrl).then(res=> res.json()).then(resdata => {
             if (resdata.data) {
             var allnums = '<div id="bb-footer">
-                <p class="bb-allnums">共 ' + resdata.data.length + ' 条 </p>
+                    < p class="bb-allnums" > 共 ' + resdata.data.length + ' 条 </p >
+                    <p class="bb-allpub"><a href="https://immmmm.com/bbs/" target="_blank">Memos Public</a></p>
             </div>'
             bbLoad.insertAdjacentHTML('afterend', allnums);
             }
